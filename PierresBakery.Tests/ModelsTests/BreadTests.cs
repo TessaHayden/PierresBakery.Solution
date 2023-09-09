c0eaab005d8e5db73523dbfb0596f6a445693624
TessaHayden <tessahayden3@gmail.com>
2023-09-08 18:00:20 -0700

    Failing TDD constructor method test.

---
+++ PierresBakery.Tests/ModelsTests/BreadTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PierresBakery.Models;
+
+namespace Bread.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
